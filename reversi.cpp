7bf33d9b3167971c4d62605aae8711a1f14d1069
Nogami Takaya <nogamitakaya@gmail.com>
2015-05-27 00:02:29 +0900

    add print Ban

--- reversi.cpp
+++ reversi.cpp
@@ -1,10 +1,57 @@
 #include <stdio.h>
+#include <string.h>
 #include "reversi.h"
 
+// *******************************************************************
+int countbit(UINT64 x)
+{
+	int ret=0;
+	for( int i=0; i<64; i++ ){
+		ret += (x&1);
+		x>>=1;
+	}
+	return ret;
+}
+
+// *******************************************************************
+// *******************************************************************
+void reversi_init()
+{
+}
+
+// *******************************************************************
+void Ban::print(UINT64 mob)
+{
+	//printf("black %016llx\n", koma[0]);
+	//printf("white %016llx\n", koma[1]);
+	printf("  A B C D E F G H\n");
+	for(int y=0; y<BAN_SIZE; y++ ){
+		printf("%d ",y+1);
+		for( int x=0; x<BAN_SIZE; x++){
+			if( mob & BIT2(x,y) ){
+				printf("+ ");
+			}else{
+				printf("%c ", "@o."[get(x,y)]);
+			}
+		}
+		printf("\n");
+	}
+	printf("BLACK=%d, WHITE=%d\n", countbit(koma[BLACK]), countbit(koma[WHITE]) );
+	const char* teban_str[2]={"BLACK","WHITE"};
+	printf("teban=%s\n", teban_str[teban]);
+}
+
+// *******************************************************************
 // *******************************************************************
 int main()
 {
 	printf("reversi\n");
+
+	reversi_init();
+	Ban  ban;
+	ban.print();
+	printf("------------------------\n");
+	ban.init();
 	
 	return 0;
 }
--- reversi.h
+++ reversi.h
@@ -1,11 +1,39 @@
 typedef unsigned long long UINT64;
+typedef int Color;
+#define  BAN_SIZE 8
 #define  BLACK 0
 #define  WHITE 1
+
+#define  BIT(x)     (1LL<<(x))
+#define  BIT2(x,y)  BIT(POS(x,y))
+#define  POS(x,y)   ((y)*BAN_SIZE+(x))
+#define  getBIT(data,x)  (((data)>>(x))&1)
 class Ban{
 	UINT64 koma[2];  //#0: Black, #1:White
+	Color    teban;
 public:
 	Ban(){ init(); }
 	~Ban(){}
+
 	void init(){
+		memset(&koma, 0, sizeof(koma));
+		put_simple(3,3,WHITE); put_simple(4,4,WHITE);
+		put_simple(4,3,BLACK); put_simple(3,4,BLACK);
+		teban=BLACK;
+
+	}
+	void print(UINT64 mob=0);
+	Color get(const int x, const int y){
+		int bitn = y*BAN_SIZE+x;
+		int bitdata = (getBIT(koma[0],bitn) + getBIT(koma[1],bitn)*2 -1)&3;
+		bitdata ^= (bitdata>>1);
+		return bitdata;
+	}
+	void put_simple(const int x, const int y, Color color){
+		koma[color] |= BIT2(x,y);
+	}
+	void put_simple(int n, Color color){
+		koma[color] |= BIT(n);
 	}
 };
+
